d60917d8f7fd5fd934e4467f983bc8d7229a45aa
Bharath Pareek <bharathpareek@gmail.com>
2013-12-06 19:44:20 -0800

    Added Comments

--- lookoutChallenge.java
+++ lookoutChallenge.java
@@ -1,3 +1,24 @@
+/*
+Program Description :
+
+Given a string S, and a list of strings of positive length, F1,R1,F2,R2,...,FN,RN, proceed to find in order the occurrences (left-to-right) of Fi in S and replace them with Ri. All strings are over alphabet { 0, 1 }. Searching should consider only contiguous pieces of S that have not been subject to replacements on prior iterations. An iteration of the algorithm should not write over any previous replacement by the algorithm.
+
+INPUT SAMPLE:
+
+Your program should accept as its first argument a path to a filename. Each line in this file is one test case. Each test case will contain a string, then a semicolon and then a list of comma separated strings. E.g.
+
+10011011001;0110,1001,1001,0,10,11
+OUTPUT SAMPLE:
+
+For each line of input, print out the string after substitutions have been made.eg.
+
+11100110
+
+Author : Bharath Kumar Pareek
+Email  : bharathpareek@gmail.com
+
+*/
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
